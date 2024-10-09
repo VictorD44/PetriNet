7c596686c0107b439fb6efced3d03061d109dd5d
Victor <vdantas440210@gmail.com>
2024-10-09 15:41:17 +0200

    Methodes jeton Place

--- src/Place.java
+++ src/Place.java
@@ -1,4 +1,17 @@
 
 public class Place {
- private int jetons;
+	private int jetons;
+ 
+ 	public int getJetons() {
+ 		return this.jetons;
+ 	}
+ 	public void setJetons(int n) {
+ 		this.jetons=n;
+ 	}
+ 	public void addJetons(int n) {
+ 		this.setJetons(this.getJetons()+n);
+ 	}
+ 	public void removeJetons(int n) {
+ 		this.setJetons(Math.max(this.getJetons()-n,0));
+ 	}
 }
